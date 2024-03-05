a4ac775e49bb530a13b49bf8ad5626b4da064e51
EktaAswal03 <155389042+EktaAswal03@users.noreply.github.com>
2024-03-05 09:01:41 +0530

    Create PythagoreanTriplet.java

---
+++ PythagoreanTriplet.java
@@ -0,0 +1,25 @@
+class Solution {
+    boolean checkTriplet(int[] arr, int n) {
+        
+        
+        if(n<=2){
+            return false;
+        }
+        // code here
+        
+        Set<Integer> array=new HashSet<Integer>();
+        
+        for(int i=0;i<n;i++){
+            arr[i]=arr[i]*arr[i];
+            array.add(arr[i]);
+        }
+        for(int i:array){
+            for(int j:array){
+                if(array.contains(i+j)){
+                    return true;
+                }
+            }
+        } 
+        return false;
+    }
+}
