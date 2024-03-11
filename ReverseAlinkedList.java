831506c5633df2253c4a79e433eb47dd69a0a7dd
EktaAswal03 <155389042+EktaAswal03@users.noreply.github.com>
2024-03-11 14:39:12 +0530

    Create ReverseAlinkedList.java

---
+++ ReverseAlinkedList.java
@@ -0,0 +1,9 @@
+ Node prev = null;
+        Node current = head;
+        while (current != null) {
+            Node next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
